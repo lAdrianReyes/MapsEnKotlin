0932a2622b48e3ea002c56d19a60baefba12969b
lAdrianReyes <l18920051@voaxaca.tecnm.mx>
2023-04-26 15:32:24 -0600

    Se agrega el proyecto

---
+++ app/src/main/java/com/example/mapsenkotlin/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.mapsenkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        createMapFragment()
+    }
+
+    private fun createMapFragment() {
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentMap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+}
